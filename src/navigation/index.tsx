5aa778cbe2a5b9f5e5b120becdbad168f72ea8a4
sean lee <seanlee0o099@gmail.com>
2020-01-11 14:10:44 +0800

    add navigation demo script

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -4,10 +4,17 @@ import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 
 const RouteConfigs = {
-  Home: createBottomTabNavigator({
+  Home: {
+    screen: createBottomTabNavigator({
       Home1: HomeScreen,
-      Login1: LoginScreen
-  }),
+      Login1: LoginScreen,
+      Home2: HomeScreen,
+      Login2: LoginScreen,
+    }),
+    navigationOptions: {
+      headerShown: false,
+    },
+  },
   Login: LoginScreen,
 };
 
